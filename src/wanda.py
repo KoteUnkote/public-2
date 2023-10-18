eb93fb23ff3c708c3c9c936d693907941b73aebc
chad-svc <chad@paidpaper.xyz>
2023-10-18 23:11:36 +0500

    pr through vscode

--- src/wanda.py
+++ src/wanda.py
@@ -6,4 +6,5 @@ print("Wanda testing v0.8.0 Preflight 0.1")
 print("Wanda testing notifications 0.2") # old local testing
 print("Wanda testing preflight 0.8.1")
 print("Wanda testing preflight 1.11.5 - migrations")
+print("Wanda testing")
 
