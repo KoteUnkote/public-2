ad18907ed283f038024692429f1009c3aa674dbf
Wanda Carlson <87515018+wanda-carlson@users.noreply.github.com>
2023-10-27 00:27:36 +0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,3 +3,11 @@ This is the main script for the script.
 """
 if __name__ == "__main__":
     print("Hello world")
+    print("Hello world")
+
+    print("Hello world")
+
+    print("Hello world")
+
+    print("Hello world")
+
